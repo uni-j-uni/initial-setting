1701091c39efa5fd925bdb55d3205fd6c0920d9a
uni-j-uni <unijun0109@gmail.com>
2025-07-18 14:36:27 +0900

    :tada:Start: 프로젝트 초기 세팅

---
+++ src/main/java/com/practice/initialsetting/InitialSettingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.initialsetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialSettingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InitialSettingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/practice/initialsetting/InitialSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.initialsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSettingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
